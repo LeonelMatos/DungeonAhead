6b7dc89a20d665a83256137c42e1e1257e9a84a1
Leonel Matos <leonellmts@gmail.com>
2022-08-21 17:30:33 +0100

    Update LinearStoryController.cs

--- Dungeon Ahead!/Assets/Scripts/Story Controller/LinearStoryController.cs
+++ Dungeon Ahead!/Assets/Scripts/Story Controller/LinearStoryController.cs
@@ -1,15 +1,25 @@
+using System.Diagnostics.Tracing;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Event
+{
+    public GameObject gameObject;
+    
+    public enum Functions {
+        Dialogue,
+        test,
+    }
+    public Functions function;
+    public bool EOS;
+
+}
 public class LinearStoryController : MonoBehaviour
 {
-    public struct Event {
-        public GameObject gameObject;
-        public enum function {
-            Dialogue,
-            test,
-        }
-        
-    }
+    public List<Event> EventList = new List<Event>();
+
+    
 }
